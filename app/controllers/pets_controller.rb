7ff2d472023dafada8d6bdeee6d12d6becb47540
Tomas Toledo <trtoledo1977@gmail.com>
2018-10-15 17:44:47 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -21,12 +21,12 @@ class PetsController < ApplicationController
 
   get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
+    @owner = Owner.all
     erb :'/pets/edit'
   end
 
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
-    @owner = Owner.all
     erb :'/pets/show'
   end
 
